30c1ecfa1c8b5e9c5d809fb32ad11bad62bcd495
yuhua he <yuhuahe@buffalo.edu>
2015-12-11 00:42:31 -0800

    url fixed

--- source/js/module.js
+++ source/js/module.js
@@ -3,7 +3,7 @@
 var app = angular.module('paymentApp', ['ui.router', 'ngStorage', 'stripe.checkout']);
 
 app.constant('ENV', {
-  API_URL: 'http://localhost:3000' || 'https://floating-thicket-4966.herokuapp.com'
+  API_URL: 'https://floating-thicket-4966.herokuapp.com'
 });
 
 app.run(function($rootScope, $localStorage) {
